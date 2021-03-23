29064b9b2a5854e37af3a07d640a37ee80d2b285
Jake <jake098877@gmail.com>
2021-03-22 23:20:37 -0400

    form built but won't submit

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const endPoint = "http://localhost:3000/api/v1/users"
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("loaded")
-    fetch(endPoint)
-    .then(resp => resp.json())
-    .then(json => console.log(json))
+    getUsers()
+   
+   const addUserForm = document.querySelector('#add-button')
+  
+   addUserForm.addEventListener('submit', (e) =>
+ 
+      eventFormHandler(e)
+ 
+      // console.log(e, "This was submitted")
+    )
   }
 )
+
+function eventFormHandler(e) {
+  // e.preventDefault()
+  debugger
+  console.log(e)
+}
+
+function getUsers() {
+  fetch(endPoint)
+    .then(resp => resp.json())
+    .then(users => {
+      const userList = document.getElementById("user-container")
+      users.data.forEach(user => {
+
+         const userMarkup = 
+         `<h3>${user.attributes.first_name} ${user.attributes.last_name}</h3>
+         <p>number_of_pianos: ${user.attributes.number_of_pianos}</p>
+         <p>phone_number: ${user.attributes.phone_number}</p>
+         <p>technician_notes: ${user.attributes.technician_notes}</p>
+        <hr>`
+       userList.innerHTML += userMarkup
+      }) 
+  })
+}

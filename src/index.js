680c78ada57a2b3cdb12a47a7471caf442376f9f
Jake <jake098877@gmail.com>
2021-04-05 12:05:32 -0400

    Finshes moving all fetch requests into Adapter class and moves event handlers into App class.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
    const addUserForm = document.querySelector('#add-user-form')
    
    addUserForm.addEventListener('submit', (e) => {
-      eventFormHandler(e)
+     const app = new App
+     app.eventFormHandler(e)
    })
   document.getElementById('user-container').addEventListener('click', e => patchSequence(e))
 })
@@ -22,7 +23,8 @@ function patchSequence(e) {
       updateUser(e)
       }
     else if(e.target.matches("button.dlt-btn")) { 
-      deleteUser(e)
+      const adapter = new Adapter;
+      adapter.fetchDelete(e.target.dataset.id)
       }
 }
 function updateUser(e) {
@@ -33,40 +35,11 @@ function updateUser(e) {
   // user form still shows
   document.getElementById('user-patch-form').addEventListener('submit', e => {
   e.preventDefault()
-  patchEventFormHandler(e)
+  const app = new App
+  app.patchEventFormHandler(e)
   }
   )}
 
-
-function patchEventFormHandler(e) {
-  e.preventDefault()
-  const id = e.target.dataset.id
-  const patchContainer = document.querySelector('#user-patch-form')
-  const inputFirstName = patchContainer.querySelector('#fname').value
-  const inputLastName = patchContainer.querySelector('#lname').value
-  const inputAddress = patchContainer.querySelector('#address').value
-  const inputPhoneNumber = patchContainer.querySelector('#pnumber').value
-  const inputNumberOfPianos = patchContainer.querySelector('#npianos').value
-  const inputNotes = patchContainer.querySelector('#notes').value
-  const fetching = new Fetching
-  fetching.patchFetch(id, inputFirstName, inputLastName, inputAddress, inputPhoneNumber,inputNumberOfPianos, inputNotes)
-}
-
-
-
-
-function eventFormHandler(e) {
-  e.preventDefault()
-  const userId = document.querySelector('#data-id')
-  const inputFirstName = document.querySelector('#input-first-name').value
-  const inputLastName = document.querySelector('#input-last-name').value
-  const inputAddress = document.querySelector('#input-address').value
-  const inputPhoneNumber = document.querySelector('#input-phone-number').value
-  const inputNumberOfPianos = document.querySelector('#input-number-of-pianos').value
-  const inputNotes = document.querySelector('#input-notes').value
-  const fetching = new Fetching
-  fetching.postFetch(inputFirstName, inputLastName, inputAddress, inputPhoneNumber,inputNumberOfPianos, inputNotes)
-}
 
 function deletePianolistener(piano) {
   const pianoContainer = document.querySelector("#piano-div")
@@ -86,8 +59,8 @@ function createPianoForm(user) {
   container.addEventListener('click', e => {
     
     if(e.target.type == 'submit') {
-      // preventDefault()
-  pianoEventFormHandler(user)
+      const app = new App
+      app.pianoEventFormHandler(user)
     }
 })
 }
@@ -109,35 +82,29 @@ function showPianos(pianos) {
   }
 }
 
-function getPianos(user, piano) {
-  fetch(`http://localhost:3000/api/v1/users/${user}/pianos/`)
-    .then(resp => resp.json())
-    .then(pianos => {
-      createPianoForm(user)
-      showPianos(pianos) 
-      })
-      .catch((err) => console.log(err))
-  
+function getPianos(user) {
+  const fetching = new Fetching;
+  fetching.getPianosFetch(user)
 }
 
-function pianoEventFormHandler(user) {
-  const container = document.querySelector('div.form-container')
-  // container.addEventListener('click', e => {
-  //   preventDefault()
-  //   if(e.target.type == 'submit') {
-  const user_id = user
-  const make = container.querySelector('#make').value
-  const model = container.querySelector('#model').value
-  const serial = container.querySelector('#serial').value
-  const age = container.querySelector('#age').value
-  const notes = container.querySelector('#notes').value
-  const image_url = container.querySelector('#image_url').value
-  // debugger
-  const fetching = new Fetching;
-  fetching.postPianoFetch(user_id, make, model, serial, age, notes, image_url)
+// function pianoEventFormHandler(user) {
+//   const container = document.querySelector('div.form-container')
+//   // container.addEventListener('click', e => {
+//   //   preventDefault()
+//   //   if(e.target.type == 'submit') {
+//   const user_id = user
+//   const make = container.querySelector('#make').value
+//   const model = container.querySelector('#model').value
+//   const serial = container.querySelector('#serial').value
+//   const age = container.querySelector('#age').value
+//   const notes = container.querySelector('#notes').value
+//   const image_url = container.querySelector('#image_url').value
+//   // debugger
+//   const fetching = new Fetching;
+//   fetching.postPianoFetch(user_id, make, model, serial, age, notes, image_url)
+// // }
+// // })
 // }
-// })
-}
 
 // function getUsers() {
 //   const adapter = new Adapter 
@@ -157,40 +124,4 @@ function createForm() {
   const option = document.createElement('option')
   option.innerText = "OPTION"
   select.appendChild(option)
-}
-
-function patchUser(id, first_name, last_name, address, phone_number, number_of_pianos, notes) {
-  const bodyData = {first_name, last_name, address, phone_number, number_of_pianos, notes}
-  fetch(`http://localhost:3000/api/v1/users/${id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(bodyData),
-  })
-  .then(resp => resp.json())
-  .then(jsonData => {
-    console.log(jsonData)
-    const user = User.findUser(id)
-    location.reload()
-    document.getElementById("user-container").innerHTML = ""
-  })
-}
-
-function deleteUser(e) {
-
-  const dataId = e.target.dataset.id
-  fetch(`http://localhost:3000/api/v1/users/${dataId}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    }
-  })
-  .then(location.reload())
-  // .then(resp => resp.json())
-  // .then(jsonData => {
-  //   getUsers()
-  // })
 }
